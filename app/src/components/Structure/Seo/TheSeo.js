fe534900005d0e69bc7e57f19b9a619e72f27905
Juliette <juliette.sageaubriot@gmail.com>
2021-03-28 17:13:58 +0200

    test: crossorigins attr

--- app/src/components/Structure/Seo/TheSeo.js
+++ app/src/components/Structure/Seo/TheSeo.js
@@ -44,8 +44,8 @@ const TheSeo = ({ title, description }) => {
       }
     >
       <link rel="manifest" href="/app.manifest" crossorigin="use-credentials"/>
-      <link rel="preload" href="/assets/fonts/fa-regular-400.woff2" as="font" type="font/woff2" />
-      <link rel="preload" href="/assets/fonts/fa-brands-400.woff2" as="font" type="font/woff2" />
+      <link rel="preload" href="/assets/fonts/fa-regular-400.woff2" as="font" type="font/woff2" crossorigin=""/>
+      <link rel="preload" href="/assets/fonts/fa-brands-400.woff2" as="font" type="font/woff2" crossorigin=""/>
     </Helmet>
   );
 }
