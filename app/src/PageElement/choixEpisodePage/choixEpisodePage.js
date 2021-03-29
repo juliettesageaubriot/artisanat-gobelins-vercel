716f9a36ed985e95b808b292993b58e9c841fb16
Aurelien.hmdy <aurelien@hemidy.fr>
2021-03-29 10:13:26 +0200

    add: menu-parallax

--- app/src/PageElement/choixEpisodePage/choixEpisodePage.js
+++ app/src/PageElement/choixEpisodePage/choixEpisodePage.js
@@ -13,23 +13,47 @@ const ChoixEpisodePage = () => {
     imagesRefVitrail.current.style.transform = `translate(-${e.clientX * .01}px, -${e.clientY * .01}px)`;
     imagesRefChapeau.current.style.transform = `translate(-${e.clientX * .01}px, -${e.clientY * .01}px)`;
   }
-
+  
   return ( 
     <section className={styles["Menu_container"]} onMouseMove={handleMouseMove}>
         <h1>Les quatres saisons de l'artisanat</h1>
 
         <div className={styles["Menu_container_parallaxSection"]}>
-          <div className={styles["vitrail"]} >
-            <img className={styles["img"]} alt="A vitrail" ref={imagesRefVitrail}/>
+          <div className={styles["vitrail"]} ref={imagesRefVitrail} >
+            <img src="assets/images/vitrail.png" className={styles['img']} alt="A vitrail"/>
+            {/* <Image
+              src="/assets/images/vitrail.png"
+              alt="A vitrail"
+              layout='fill'
+              className={styles['img']}
+            /> */}
           </div>
-          <div className={styles["violon"]}>
-            <img className={styles["img"]} alt="A violon" ref={imagesRefViolon}/>
+          <div className={styles["violon"]} ref={imagesRefViolon}>
+            <img src="/assets/images/violon.png" className={styles["img"]} alt="A violon"/>
+            {/* <Image
+              src="/assets/images/violon.png"
+              alt="A violon"
+              layout='fill'
+              className={styles['img']}
+            /> */}
           </div>
-          <div className={styles["chapeau"]}>
-            <img className={styles["img"]} alt="A hat" ref={imagesRefChapeau}/>
+          <div className={styles["chapeau"]} ref={imagesRefChapeau}>
+            <img src="/assets/images/chapeau.png" className={styles["img"]} alt="A hat"/>
+            {/* <Image
+              src="/assets/images/chapeau.png"
+              alt="A hat"
+              layout='fill'
+              className={styles['img']}
+            /> */}
           </div>
-          <div className={styles["bijoux"]}>
-            <img className={styles["img"]} alt="A bijoux" ref={imagesRefBijoux}/>
+          <div className={styles["bijoux"]} ref={imagesRefBijoux}>
+            <img src="/assets/images/bijoux.png" className={styles["img"]} alt="A bijoux"/>
+            {/* <Image
+              src="/assets/images/bijoux.png"
+              alt="A bijoux"
+              layout='fill'
+              className={styles['img']}
+            /> */}
           </div>
         </div>
         
