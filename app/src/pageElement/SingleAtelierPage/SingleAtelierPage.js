333c7f27730ef37441cb77ef42a8b00329494548
Juliette <juliette.sageaubriot@gmail.com>
2021-03-31 21:05:50 +0200

    feat: add promise in gltf loader, c'est du sale

--- app/src/pageElement/SingleAtelierPage/SingleAtelierPage.js
+++ app/src/pageElement/SingleAtelierPage/SingleAtelierPage.js
@@ -46,6 +46,20 @@ const SingleAtelierPage = () => {
       load: false
     }
 
+    //COLORPICKER CURRENT COLOR
+    const colorPicked = {
+      current: null,
+      old: null
+    }
+
+    /**
+    * Sizes
+    */
+    const sizes = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+
     let mixer
     let currentCamera
     let cameraClip
@@ -58,238 +72,230 @@ const SingleAtelierPage = () => {
       )
     }
 
-    Promise.all(Data.map(buildScene)).then((objects) => {
-      objects.map((elm, i) => {
-        let parent = elm.scene.children[0]
-        let parentName = elm.scene.children[0].name
+    console.log('entré')
 
-        if ("colorPickerGroup" === parentName) {
-          vitrailGroup.add(parent)
-          SetupColorPicker(parent, objectToTest, vitrailObjects)
-        } else if ("atelierGroup" === parentName) {
-          atelierGroup.add(parent)
-          SetupAtelier()
-          // console.log(parent.children);
-        }
+    const dataMap = async () => {
+      console.log('pendant')
 
-        if ("atelierCamGroup" === parentName) {
-          mixer = new THREE.AnimationMixer(elm.scene)
-          cameraClip = mixer.clipAction(elm.animations[0])
-          // console.log(cameraClip);
+      await Promise.all(Data.map(buildScene)).then((objects) => {
+        objects.map((elm, i) => {
+          let parent = elm.scene.children[0]
+          let parentName = elm.scene.children[0].name
 
-          currentCamera = elm.cameras[0]
-          // console.log(currentCamera);
-        }
+          if ("colorPickerGroup" === parentName) {
+            vitrailGroup.add(parent)
+            SetupColorPicker(parent, objectToTest, vitrailObjects)
+          } else if ("atelierGroup" === parentName) {
+            atelierGroup.add(parent)
+            SetupAtelier()
+            // console.log(parent.children);
+          }
+
+          if ("atelierCamGroup" === parentName) {
+            mixer = new THREE.AnimationMixer(elm.scene)
+            cameraClip = mixer.clipAction(elm.animations[0])
+
+            currentCamera = elm.cameras[0]
+            // console.log(currentCamera);
+          }
+        })
       })
-      params.load = true
-      console.log("InsideCurrentCamera", currentCamera);
-      console.log("inside", params.load);
-    })
 
-    console.log("outside", params.load);
-    console.log("OutsideCurrentCamera", currentCamera);
+      console.log('pendant2')
 
-    // Elements positions
-    vitrailGroup.position.set(-1.5, 1.2, 2.2)
-    vitrailGroup.rotation.set(0, Math.PI / 2, 0)
-    vitrailGroup.scale.set(0.7, 0.7, 0.7)
+      console.log('sortie');
 
-    // console.log(params.load);
 
-    // if (true === params.load) {
-    //   console.log(vitrailGroup.children[0]);
-    // }
+      // console.log("outside", params.load);
+      // console.log("OutsideCurrentCamera", currentCamera);
 
-    //COLORPICKER CURRENT COLOR
-    const colorPicked = {
-      current: null,
-      old: null
-    }
+      // Elements positions
+      vitrailGroup.position.set(-1.5, 1.2, 2.2)
+      vitrailGroup.rotation.set(0, Math.PI / 2, 0)
+      vitrailGroup.scale.set(0.7, 0.7, 0.7)
 
-    let isMouseDown = false;
-    const handleMouseDown = () => {
-      isMouseDown = true;
-      if (currentIntersect) {
-        switch (currentIntersect.object.name) {
-          case "green":
-            colorPicked.current = currentIntersect.object.material.color;
-            cursorColorPickerInner.current.setAttribute("data-color-cursor", "green");
-            cursorColorPickerInner.current.style.transform = "scale(1.5)"
-            break
-          case "purple":
-            colorPicked.current = currentIntersect.object.material.color;
-            cursorColorPickerInner.current.setAttribute("data-color-cursor", "purple");
-            cursorColorPickerInner.current.style.transform = "scale(1.5)"
-            break
-          case "white":
-            colorPicked.current = currentIntersect.object.material.color;
-            cursorColorPickerInner.current.setAttribute("data-color-cursor", "white");
-            cursorColorPickerInner.current.style.transform = "scale(1.5)"
-            break
+
+      let isMouseDown = false;
+      const handleMouseDown = () => {
+        isMouseDown = true;
+        if (currentIntersect) {
+          switch (currentIntersect.object.name) {
+            case "green":
+              colorPicked.current = currentIntersect.object.material.color;
+              cursorColorPickerInner.current.setAttribute("data-color-cursor", "green");
+              cursorColorPickerInner.current.style.transform = "scale(1.5)"
+              break
+            case "purple":
+              colorPicked.current = currentIntersect.object.material.color;
+              cursorColorPickerInner.current.setAttribute("data-color-cursor", "purple");
+              cursorColorPickerInner.current.style.transform = "scale(1.5)"
+              break
+            case "white":
+              colorPicked.current = currentIntersect.object.material.color;
+              cursorColorPickerInner.current.setAttribute("data-color-cursor", "white");
+              cursorColorPickerInner.current.style.transform = "scale(1.5)"
+              break
+          }
         }
       }
-    }
-    const handleMouseUp = () => {
-      isMouseDown = false;
-      if (currentIntersect) {
-        if (vitrailObjects.includes(currentIntersect.object.name)) {
-          currentIntersect.object.material.color = colorPicked.current;
+      const handleMouseUp = () => {
+        isMouseDown = false;
+        if (currentIntersect) {
+          if (vitrailObjects.includes(currentIntersect.object.name)) {
+            currentIntersect.object.material.color = colorPicked.current;
+          }
+          colorPicked.current = null;
+          cursorColorPickerInner.current.setAttribute("data-color-cursor", "default");
+          cursorColorPickerInner.current.style.transform = "scale(.8)"
+        } else {
+          colorPicked.current = null;
+          cursorColorPickerInner.current.setAttribute("data-color-cursor", "default");
+          cursorColorPickerInner.current.style.transform = "scale(.8)"
         }
-        colorPicked.current = null;
-        cursorColorPickerInner.current.setAttribute("data-color-cursor", "default");
-        cursorColorPickerInner.current.style.transform = "scale(.8)"
-      } else {
-        colorPicked.current = null;
-        cursorColorPickerInner.current.setAttribute("data-color-cursor", "default");
-        cursorColorPickerInner.current.style.transform = "scale(.8)"
       }
-    }
 
-    const mouse = new THREE.Vector2()
+      const mouse = new THREE.Vector2()
 
-    const handleMouseMove = (event) => {
-      mouse.x = event.clientX / sizes.width * 2 - 1
-      mouse.y = - (event.clientY / sizes.height) * 2 + 1
-      // cursorColorPickerContainer.current.style.transform = `translate(${event.clientX - 25}px, ${event.clientY - 25}px)`;
-      // console.log(mouse)
-    }
+      const handleMouseMove = (event) => {
+        mouse.x = event.clientX / sizes.width * 2 - 1
+        mouse.y = - (event.clientY / sizes.height) * 2 + 1
+        // cursorColorPickerContainer.current.style.transform = `translate(${event.clientX - 25}px, ${event.clientY - 25}px)`;
+        // console.log(mouse)
+      }
 
-    window.addEventListener('mousemove', handleMouseMove)
+      window.addEventListener('mousemove', handleMouseMove)
 
-    // window.addEventListener('click', handleClickOnObjects)
+      // window.addEventListener('click', handleClickOnObjects)
 
-    window.addEventListener('pointerdown', handleMouseDown)
+      window.addEventListener('pointerdown', handleMouseDown)
 
-    window.addEventListener('pointerup', handleMouseUp)
+      window.addEventListener('pointerup', handleMouseUp)
 
-    /**
-  * Lights
-  */
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
-    scene.add(ambientLight)
+      /**
+    * Lights
+    */
+      const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+      scene.add(ambientLight)
 
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
-    directionalLight.castShadow = true
-    directionalLight.shadow.mapSize.set(1024, 1024)
-    directionalLight.shadow.camera.far = 15
-    directionalLight.shadow.camera.left = - 7
-    directionalLight.shadow.camera.top = 7
-    directionalLight.shadow.camera.right = 7
-    directionalLight.shadow.camera.bottom = - 7
-    directionalLight.position.set(5, 5, 5)
-    scene.add(directionalLight)
-
-    /**
-* Sizes
-*/
-    const sizes = {
-      width: window.innerWidth,
-      height: window.innerHeight
-    }
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+      directionalLight.castShadow = true
+      directionalLight.shadow.mapSize.set(1024, 1024)
+      directionalLight.shadow.camera.far = 15
+      directionalLight.shadow.camera.left = - 7
+      directionalLight.shadow.camera.top = 7
+      directionalLight.shadow.camera.right = 7
+      directionalLight.shadow.camera.bottom = - 7
+      directionalLight.position.set(5, 5, 5)
+      scene.add(directionalLight)
 
 
-    // Resize
-    window.addEventListener('resize', () => {
-      // Update sizes
-      sizes.width = window.innerWidth
-      sizes.height = window.innerHeight
+      // Resize
+      window.addEventListener('resize', () => {
+        // Update sizes
+        sizes.width = window.innerWidth
+        sizes.height = window.innerHeight
 
-      // Update camera
-      camera.aspect = sizes.width / sizes.height
-      camera.updateProjectionMatrix()
+        // Update camera
+        camera.aspect = sizes.width / sizes.height
+        camera.updateProjectionMatrix()
 
-      // Update renderer
+        // Update renderer
+        renderer.setSize(sizes.width, sizes.height)
+        renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+      })
+
+      /**
+      * Camera
+      */
+      // Base camera
+
+      // const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+      const camera = new THREE.PerspectiveCamera(currentCamera.fov, sizes.width / sizes.height, currentCamera.near, currentCamera.far)
+      camera.position.set(- .3, 2, 2.2)
+      camera.lookAt(vitrailGroup.position)
+      scene.add(camera)
+
+      // Controls
+      // const controls = new OrbitControls(camera, canvas)
+      // controls.target.set(0, 1, 0)
+      // controls.enableDamping = true
+      // controls.enabled = true
+
+
+      /**
+   * Renderer
+   */
+      const renderer = new THREE.WebGLRenderer()
+      renderer.shadowMap.enabled = true
+      renderer.shadowMap.type = THREE.PCFSoftShadowMap
       renderer.setSize(sizes.width, sizes.height)
       renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    })
-
-    /**
-    * Camera
-    */
-    // Base camera
-
-    const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-    // const camera = new THREE.PerspectiveCamera(currentCamera.fov, sizes.width / sizes.height, currentCamera.near, currentCamera.far)
-
-    camera.position.set(- .3, 2, 2.2)
-    camera.lookAt(vitrailGroup.position)
-    scene.add(camera)
-
-    // Controls
-    // const controls = new OrbitControls(camera, canvas)
-    // controls.target.set(0, 1, 0)
-    // controls.enableDamping = true
-    // controls.enabled = true
+      canvas.appendChild(renderer.domElement);
 
 
-    /**
- * Renderer
- */
-    const renderer = new THREE.WebGLRenderer()
-    renderer.shadowMap.enabled = true
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-    canvas.appendChild(renderer.domElement);
+      const clock = new THREE.Clock()
+      let previousTime = 0
+      let currentIntersect = null
 
+      const tick = () => {
+        const elapsedTime = clock.getElapsedTime()
+        const deltaTime = elapsedTime - previousTime
+        previousTime = elapsedTime
 
-    const clock = new THREE.Clock()
-    let previousTime = 0
-    let currentIntersect = null
+        //Raycast
+        raycaster.setFromCamera(mouse, camera);
 
-    const tick = () => {
-      const elapsedTime = clock.getElapsedTime()
-      const deltaTime = elapsedTime - previousTime
-      previousTime = elapsedTime
+        const intersects = raycaster.intersectObjects(objectToTest)
 
-      //Raycast
-      raycaster.setFromCamera(mouse, camera);
-
-      const intersects = raycaster.intersectObjects(objectToTest)
-
-      if (intersects.length) {
-        if (currentIntersect && currentIntersect !== intersects[0]) {
-          if (isMouseDown === true) {
-            currentIntersect.object.material.color = colorPicked.old;
+        if (intersects.length) {
+          if (currentIntersect && currentIntersect !== intersects[0]) {
+            if (isMouseDown === true) {
+              currentIntersect.object.material.color = colorPicked.old;
+            }
           }
-        }
-        currentIntersect = intersects[0]
-        // console.log('mouse enter')
-        colorPicked.old = currentIntersect.object.material.color;
-        if (isMouseDown === true && vitrailObjects.includes(currentIntersect.object.name)) {
-          currentIntersect.object.material.color = colorPicked.current;
-        }
-        // console.log(intersects);
-
-      }
-      else {
-        if (currentIntersect) {
-          // console.log('mouse leave')
-          if (isMouseDown === true) {
-            currentIntersect.object.material.color = colorPicked.old;
+          currentIntersect = intersects[0]
+          // console.log('mouse enter')
+          colorPicked.old = currentIntersect.object.material.color;
+          if (isMouseDown === true && vitrailObjects.includes(currentIntersect.object.name)) {
+            currentIntersect.object.material.color = colorPicked.current;
           }
-          colorPicked.old = null;
-          // console.log(currentIntersect.object.name);
+          // console.log(intersects);
 
         }
+        else {
+          if (currentIntersect) {
+            // console.log('mouse leave')
+            if (isMouseDown === true) {
+              currentIntersect.object.material.color = colorPicked.old;
+            }
+            colorPicked.old = null;
+            // console.log(currentIntersect.object.name);
 
-        currentIntersect = null
+          }
+
+          currentIntersect = null
+        }
+
+        // Camera
+        // camera.lookAt(vitrailGroup.position)
+
+        // Update controls
+        // controls.update()
+
+        // Render
+        renderer.render(scene, camera)
+
+        // Call tick again on the next frame
+        window.requestAnimationFrame(tick)
       }
 
-      // Camera
-      // camera.lookAt(vitrailGroup.position)
+      tick()
 
-      // Update controls
-      // controls.update()
-
-      // Render
-      renderer.render(scene, camera)
-
-      // Call tick again on the next frame
-      window.requestAnimationFrame(tick)
     }
 
-    tick()
+    dataMap()
+
+
 
   }, [])
 
