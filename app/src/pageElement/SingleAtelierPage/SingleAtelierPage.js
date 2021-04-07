e5ae9a65d5015c0b2e1a7f028ed1f5bf73501715
Juliette <juliette.sageaubriot@gmail.com>
2021-04-07 14:03:58 +0200

    feat: update lights

--- app/src/pageElement/SingleAtelierPage/SingleAtelierPage.js
+++ app/src/pageElement/SingleAtelierPage/SingleAtelierPage.js
@@ -71,6 +71,14 @@ const SingleAtelierPage = () => {
         console.log(objects);
         objects.map((gltf, i) => {
           gltf.scene.traverse(child => {
+
+            if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+              // child.material.envMap = environmentMap
+              // child.material.envMapIntensity = 5
+              child.castShadow = true
+              child.receiveShadow = true
+            }
+
             if ("colorPickerGroup" === child.name) {
               vitrailGroup.add(child);
               SetupColorPicker(child, objectToTest, vitrailObjects);
@@ -160,18 +168,16 @@ const SingleAtelierPage = () => {
       /**
     * Lights
     */
-      const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
-      scene.add(ambientLight)
 
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6)
+       const ambientLight = new THREE.AmbientLight(0xffffff, 0.8)
+       scene.add(ambientLight)
+
+      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
+      directionalLight.position.set(0.25, 3, -2.25,)
       directionalLight.castShadow = true
-      directionalLight.shadow.mapSize.set(1024, 1024)
       directionalLight.shadow.camera.far = 15
-      directionalLight.shadow.camera.left = - 7
-      directionalLight.shadow.camera.top = 7
-      directionalLight.shadow.camera.right = 7
-      directionalLight.shadow.camera.bottom = - 7
-      directionalLight.position.set(5, 5, 5)
+      directionalLight.shadow.mapSize.set(1024, 1024)
+      directionalLight.shadow.normalBias = 0.05
       scene.add(directionalLight)
 
       /**
