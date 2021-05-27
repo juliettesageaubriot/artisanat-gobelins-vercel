c860aeaa422c7b4d0ab1a02e6d81329f50a01fc2
Juliette <juliette.sageaubriot@gmail.com>
2021-05-27 15:19:09 +0200

    feat: add double slider scroll mecanism

--- app/src/pageElement/FinEpisode/FinEpisodePage.js
+++ app/src/pageElement/FinEpisode/FinEpisodePage.js
@@ -1,138 +1,154 @@
-import { useEffect, useRef, useState } from "react";
-import ThePrimaryButton from "@components/PrimaryButton/ThePrimaryButton";
-// import TheVolume from "@components/VolumeSettings/TheVolume";
+import React, { useEffect, useRef, useState } from "react";
+import Slider from "react-slick";
+
+import TheModal from '@components/Modal/TheModal'
+import useModal from '@hooks/useModal'
+
+import data from '@assets/data/interviews.json'
+
 import styles from "./styles.module.scss";
 
 const FinEpisodePage = () => {
-  const [indexCarousel, setIndexCarousel] = useState(2)
-  let positionOneX;
-  let positionOneY;
-  let positionTwoX;
-  let positionTwoY;
-  let positionThreeX;
-  let positionThreeY;
+  const { isShowing: isShowingAbout, toggle: toggleAbout } = useModal();
 
-  const carouselContainer = useRef(null);
-  const imageCarousel = useRef(null);
-  const labelCarouselOne = useRef(null);
-  const labelCarouselTwo = useRef(null);
-  const labelCarouselThree = useRef(null);
+  // const slider = useRef();
+  const sliderRef = useRef();
+  const parentRef = useRef(null);
+
+  const [nav1, setNav1] = useState(null);
+  const [nav2, setNav2] = useState(null);
+
+  let slider1 = []
+  let slider2 = []
+
+
+  const modalTextAbout = [{
+    title: "À propos",
+    slug: "Crédits",
+    content: "Ce projet a été réalisé dans le cadre du master de design et développement de l'innovation interactive. Il a été crée par deux développeurs <span>Juliette Sage--Aubriot, Aurélien Hémidy</span> et trois designers <span>Éloïse Luna, Vincent Calas et Chloélia Breton.</span>",
+    credits: "Un merci particulier pour tous les intervenants qui nous ont aidés tout au long du projet, à la comédienne <span>Roxane Fomberteau</span> qui a prêté sa voix à notre maître d'apprentissage, à <span>Louise Doublet</span> qui nous a accompagné dans la découverte du métier de vitrailliste, et à <span>Adrien Melchior</span> pour la création musicale.",
+    typographies: "Typographies : cirka, fonderie pangram pangram",
+    gobelins: '/assets/images/logo/logo_gobelins.png',
+    buttons: false
+  }]
+
+  useEffect(() => {
+    setNav1(slider1)
+    setNav2(slider2)
+  }, [slider1, slider2,])
+
+  const settings1 = {
+    dots: true,
+    infinite: true,
+    // speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true
+  };
+
+  const settings2 = {
+    infinite: true,
+    // speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    vertical: true
+  };
 
 
   useEffect(() => {
-    getNewCoordinates(labelCarouselOne, labelCarouselTwo, labelCarouselThree);
-  }, [])
+    if (!parentRef.current) {
+      return;
+    }
+    parentRef.current.addEventListener('wheel', (e) => { handleScroll(e) });
+    return (() => {
+      parentRef.current.removeEventListener('wheel', (e) => handleScroll(e));
+    });
+  }, [parentRef]);
 
-  const getNewCoordinates = (previousLabel, currentLabel, nextLabel) => {
-    positionOneX = 20;
-    positionOneY = carouselContainer.current.getBoundingClientRect().top - carouselContainer.current.getBoundingClientRect().top + 20
 
-    positionTwoX = imageCarousel.current.getBoundingClientRect().left - currentLabel.current.getBoundingClientRect().left;
-    positionTwoY = imageCarousel.current.getBoundingClientRect().top - carouselContainer.current.getBoundingClientRect().top - currentLabel.current.getBoundingClientRect().height;
-
-    positionThreeX = 20;
-    positionThreeY = carouselContainer.current.getBoundingClientRect().bottom - carouselContainer.current.getBoundingClientRect().top - nextLabel.current.getBoundingClientRect().height;
-
-    previousLabel.current.style.transform = `translate3d(${positionOneX}px, ${positionOneY}px, 0) scale(1)`;
-    currentLabel.current.style.transform = `translate3d(${positionTwoX}px, ${positionTwoY}px, 0) scale(1.5)`;
-    nextLabel.current.style.transform = `translate3d(${positionThreeX}px, ${positionThreeY}px, 0) scale(1)`;
-
-    previousLabel.current.style.opacity = .5;
-    currentLabel.current.style.opacity = 1;
-    nextLabel.current.style.opacity = .5;
-
-    setTimeout(() => {
-      setTransitions();
-    }, 400);
+  const handleScroll = (e) => {
+    console.log(e);
+    if (e.deltaY > 0) {
+      nav1 && nav1.slickPrev();
+      nav2 && nav2.slickPrev();
+    } else if (e.deltaY < 0) {
+      nav1 && nav1.slickNext();
+      nav2 && nav2.slickNext();
+    }
   };
 
-  const setTransitions = () => {
-    labelCarouselOne.current.style.transition = ".3s transform ease-in-out, .5s opacity ease-in-out";
-    labelCarouselTwo.current.style.transition = ".3s transform ease-in-out, .5s opacity ease-in-out";
-    labelCarouselThree.current.style.transition = ".3s transform ease-in-out, .5s opacity ease-in-out";
-  }
-
-  const handleLabelOneCurrent = () => {
-    //label ONE CURRENT
-    if(indexCarousel === 1) 
-      return
-    setIndexCarousel(1);
-    getNewCoordinates(labelCarouselThree, labelCarouselOne, labelCarouselTwo);
-  };
-
-  const handleLabelTwoCurrent = () => {
-    //label THREE CURRENT
-    if(indexCarousel === 2) 
-      return
-    setIndexCarousel(2);
-    getNewCoordinates(labelCarouselOne, labelCarouselTwo, labelCarouselThree);
-  };
-
-  const handleLabelThreeCurrent = () => {
-    //label TWO CURRENT
-    if(indexCarousel === 3) 
-      return
-    setIndexCarousel(3);
-    getNewCoordinates(labelCarouselTwo, labelCarouselThree, labelCarouselOne);
-  };
-  
 
   return (
     <section className={styles["page-fin"]}>
-      <div className={styles["page-fin__carousel-container"]}>
-          <h1 className={styles["page-fin__heading"]}> De l'atelier d'initiation...</h1>
-          <div className={styles["carousel-inner"]}>
-            <div className={styles["carousel-inner__counter"]}>
-              <div className={styles["counter"]}>
-                <p>{ indexCarousel }</p>
-                <div className={styles["counter__separator"]}></div>
-                <p>2</p>
-              </div>
-              <img src="assets/images/carousel-event.png" alt=""/>
-            </div>
-            <div className={styles["carousel-inner__content"]} ref={carouselContainer}>
-              <p ref={labelCarouselOne} onClick={handleLabelOneCurrent}>“ Maintenant notre métier n'est pas si old school qu'on voudrait le laisser croire ” 1</p>
-              <p ref={labelCarouselTwo} onClick={handleLabelTwoCurrent}>" C'est dommage que [l’artisanat] soit encore aux yeux des conseillers d'orientation la voie de secours " 2</p>
-              <p ref={labelCarouselThree} onClick={handleLabelThreeCurrent}>“ On dit dans le métier qu'il faut une dizaine d'années, et c'est pas avec la réduction des heures de formation qu'on tend vers cela ” 3</p>
-              <img src="assets/images/vitrailliste.png" className={styles["vitrailliste"]} alt=""/>
-              <img className={styles["background"]} ref={imageCarousel} alt=""/>
-            </div>
-          </div>
-          <div className={styles["page-fin__footer-container"]}>
-              <ThePrimaryButton label="Episodes" to={"/"} active={false}/>
-              <ThePrimaryButton label="Episode 2, le joailler ->" to={"/"} active={false} />
-              {/* <TheVolume absolute={false} colorPicto="blanc"/> */}
-          </div>
-      </div>
-      <div className={styles["page-fin__video-container"]}>
-        <h1 className={`${styles["page-fin__heading"]} ${styles["--white"]}`}> ... A la rencontre des professionnels</h1>
-        <div className={styles["video-inner"]}>
-          <h4 className={styles["video-inner__subHeading"]}>Une dizaine d'années pour devenir vitrailliste ?</h4>
-          <div className={styles["video-inner__content"]}>
-            <div className={styles["avatar"]}>
-              <div className={styles["avatar__img"]}>
-                <img src="" alt=""/>
-              </div>
-              <h6>Marion Gosselin</h6>
-              <p>Vitrailliste à l'atelier de Paris</p>
-            </div>
-            <div className={styles["avatar"]}>
-              <div className={styles["avatar__img"]}>
-                <img src="" alt=""/>
-              </div>
-              <h6>Louise Doublet</h6>
-              <p>étudiante DNMADE à l'ENSAAMA</p>
-            </div>
-            <div className={styles["avatar"]}>
-              <div className={styles["avatar__img"]}>
-                <img src="" alt=""/>
-              </div>
-              <h6>Maxime Gauthier</h6>
-              <p>maître d'apprentissage à l'atelier de Chartres</p>
+      <div className={styles["page-fin_container"]}>
+
+        <div className={styles.head}>
+          <h1>De l'atelier d'initiation... <br /> <span>À la rencontre des professionnels</span></h1>
+          <div className={`${styles["btn_container"]}`}>
+            <div className={`${styles["btn__inner"]}`}>
+              <button className={`btn btn-about ${isShowingAbout === true && styles.disabled}`} onClick={toggleAbout}><span>À propos</span></button>
             </div>
           </div>
         </div>
+
+
+        <div
+        className={styles.sliders}
+          ref={parentRef}
+        >
+          <Slider
+            {...settings1}
+            asNavFor={nav2}
+
+            // ref={sliderRef}
+            ref={slider => (slider1 = slider)}
+          >
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+          </Slider>
+
+          <Slider
+            {...settings2}
+            asNavFor={nav1}
+
+            // ref={sliderRef}
+            ref={slider => (slider2 = slider)}
+
+            swipeToSlide={true}
+            focusOnSelect={true}
+          >
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+          </Slider>
+
+        </div>
+
+
+        <div className={styles.footer}>
+          <div className={`${styles["btn_container"]}`}>
+            <a href="/" className={`link link-secondary link-black ${styles['link-before']} ${styles['link-black']}`}>
+              <span>Épisodes</span>
+            </a>
+          </div>
+        </div>
       </div>
+
+      <TheModal isShowing={isShowingAbout} hide={toggleAbout} content={modalTextAbout} />
+      <div className={`${styles['overlay-gribouillis']}`}></div>
     </section>
   )
 }
